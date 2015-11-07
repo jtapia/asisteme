6f116af49761f92d6f0b8e27842b575830312c20
JOSE HERIBERTO <heriberto.perez@magmalabs.io>
2015-11-07 17:00:15 -0600

    Adds two different routes for sign-up:   - /nurses/sign_up   - /customers/sign_up
    - Customizes login, signup and forgot password devise's views
    - Allows type field param when the users signs up
    - Adds login and logout options in the home page
    - Changes title name and icon in registration page depending on type of user
    - Creates different type of users using STI through the usage of the field "type"
    - Sets customer as a default user type when is not provided.
    - Adds spanish translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      assign_user_type
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :type) }
+    end
+
+    def assign_user_type
+      if params[:user] && params[:user][:type].blank?
+        params[:user][:type] = session[:registration]
+      end
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def set_user_registration_type_session
+    session[:registration] = "Customer" if request.fullpath =~ /\/customers\/sign_up/
+    session[:registration] = "Nurse" if request.fullpath =~ /\/nurses\/sign_up/
+    ''
+  end
+
+  def format_user_form_title
+    content_tag :div, class: 'registration-title-container s12 m12 l12 center' do
+
+      if session[:registration] == 'Nurse'
+        icon = 'local_hospital'
+        person_title = 'Enfermera'
+      end
+
+      if session[:registration] == 'Customer'
+        icon = 'person'
+        person_title = 'Cliente'
+      end
+
+      concat(content_tag(:p, class: 'light-blue-text text-lighten-3 icon-title') do
+        "<i class='material-icons icon-increase-font-size'>#{icon}</i>".html_safe
+      end)
+
+      concat(content_tag(:h3, class: 'center light-blue-text text-lighten-3 title') do
+        person_title
+      end)
+    end
+
+  end
 end
