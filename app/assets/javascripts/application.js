ae410caa9c5cd7fdc8be293c818f61506ad840ec
Gustavo Robles <gustavo.robles@magmalabs.io>
2015-11-07 06:00:19 -0600

    add materialize design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require materialize
+//= require materialize/extras/nouislider
 //= require turbolinks
 //= require underscore.js
 //= require_tree .
